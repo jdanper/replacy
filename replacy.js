fd6ef6e63d15cb516e397af5314820ea562f297c
Daniel Pereira <danieldsw2@gmail.com>
2017-03-31 17:00:44 -0300

    fix command-line command

--- replacy.js
+++ replacy.js
@@ -7,7 +7,7 @@ var path = require('path');
 var replace = require('replace');
 
 program
-  .version('0.0.2')
+  .version('0.0.4')
   .command('<expression> <replacement>')
   .parse(process.argv);
 
