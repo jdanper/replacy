305558bd61cf1515e11310b7eb723501fd2b1e3f
Daniel Pereira <danieldsw2@gmail.com>
2017-09-05 15:33:45 -0300

    add capability to use as module and call its methods from js code

---
+++ replacy.js
@@ -0,0 +1,72 @@
+#!/usr/bin/env node
+'use strict';
+
+const program = require('commander');
+var fs = require('fs');
+var path = require('path');
+var replace = require('replace');
+
+program
+    .version('0.1.0')
+    .command('<expression> <replacement>')
+    .parse(process.argv);
+
+
+var replacement = ''
+var exp = '';
+
+if (process.argv.length >= 4) {
+    exp = process.argv[2];
+    replacement = process.argv[3];
+    var rootPath = process.cwd();
+
+    console.log('Replacing ' + exp + ' with ' + replacement + '.');
+    console.log('Using ' + rootPath + ' as root.');
+
+    console.log('>> Directories <<');
+    rename(rootPath, false);
+
+    console.log('>> Files <<');
+    rename(rootPath, true);
+    console.log('>> Replacing inside files <<');
+    replaceInside(rootPath);
+} else {
+    console.log('Must pass terms.');
+    process.exit;
+}
+
+module.exports = {
+    rename: function rename(dir, justFiles) {
+        var flst = fs.readdirSync(dir);
+
+        flst.forEach(file => {
+            if (file.includes('.git') || file.includes('app.js')) return;
+
+            var finalPath = path.join(dir, file);
+            var nfile = file.replace(exp, replacement);
+            var endPoint = path.join(dir, nfile);
+
+            if (fs.statSync(finalPath).isDirectory()) {
+                rename(finalPath, justFiles);
+                if (!justFiles)
+                    if (file.includes(exp)) {
+                        console.log(finalPath + " -> " + nfile);
+                        fs.renameSync(finalPath, endPoint);
+                    }
+            } else if (justFiles && file.includes(exp)) {
+                console.log(finalPath + " -> " + nfile);
+                fs.renameSync(finalPath, endPoint);
+            }
+        });
+    },
+
+    relace: function replaceInside(path) {
+        replace({
+            regex: exp,
+            replacement: replacement,
+            paths: [path],
+            recursive: true,
+            silent: true,
+        });
+    }
+}
